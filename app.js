601558d282c3bfeff63291dde16c97138f0b003a
Yash Bansal <yashbansal@Yashs-MacBook-Pro.local>
2025-12-06 19:04:54 +0530

    some work

--- app.js
+++ app.js
@@ -1,72 +1,15 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const usersRouter = require('./routes/usersRoute');
+
 const app = express();
-const fs = require('fs');
-const jwt = require('jsonwebtoken');
-
-const saltRounds = process.env.SALT_ROUNDS || 10;
-const jwtSecret = process.env.JWT_SECRET || 'super_strong_64char';
-
-const data = fs.readFileSync('data.json');
-const userData = JSON.parse(data);
-const verifyUser = require('./middleware').verifyUser;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(verifyUser);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
-app.post('/register', (req, res) => {
-    const receivedData = req.body;
-    const plainPassword = receivedData.password;
-
-    bcrypt.hash(plainPassword, saltRounds, (err, hashedPassword) => {
-        if (err) {
-            return res.status(500).json({ error: 'Error hashing password' });
-        }
-        receivedData.password = hashedPassword;
-        let userModel = {
-                "id": receivedData.id || Date.now().toString(),
-                "password": hashedPassword
-            };
-        userData[receivedData.username] = userModel;        
-        res.status(201).json({
-            message: 'User registered successfully',
-            userData: receivedData.username
-        });
-        console.log(userData);
-    });
-});
-
-app.post('/login', (req, res) => {
-    const user = req.body;
-    const storedUser = userData[user.username];
-
-    if (!storedUser) {
-        return res.status(404).json({ error: 'User not found' });
-    }
-
-    bcrypt.compare(user.password, storedUser.password, (err, result) => {
-        if (err) {
-            return res.status(500).json({ error: 'Error comparing passwords' });
-        }
-        if (result) {
-            const token = jwt.sign(
-                {
-                    sub: storedUser.id,
-                    username: user.username
-                },
-                jwtSecret,
-                { expiresIn: '1h' }
-            );
-            res.json({ message: 'Login successful', token: token });
-        } else {
-            res.status(401).json({ error: 'Invalid username/password' });
-        }
-    });
-});
+app.use('/users', usersRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-const express = require('express');
-const app = require('./app');
-const port = 3000;
+const app = require('./app'); 
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
-    }
-    console.log(`Server is listening on ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
+
+module.exports = app;
